f1db7674da5858fbb185017ec457c1a3271ebf5b
Manoryk Hanna <86349455+anna-manorik@users.noreply.github.com>
2025-02-28 16:27:10 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.mjs'
+
+const description = document.getElementById("description");
+const priority = document.getElementById("priority");
+const statusTodo = document.getElementById("status");
+const addTaskBtn = document.getElementById("addTaskBtn");
+
+
+addTaskBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+    const newTodo = {
+        id: nanoid(),
+        description: description.value,
+        priority: priority.value,
+        status: statusTodo.value
+    }
+
+    // console.log("todoList", todoList)
+    localStorage.setItem("todoList", JSON.stringify([newTodo, ...todoList]));
+})
